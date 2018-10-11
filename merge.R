d76db2bdeaaaa0b0ef0b6d2a255becf2ca95e5d8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 10:40:35 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -13,6 +13,6 @@ arrests
 
 #3) Create a merged dataframe -- with the attributes from both dataset
 
-merge_df <- merge(arrests,df,by="Statename")
+merge_df <- merge(arrests,df,by="Statename", x.all=TRUE)
 
 merge_df
